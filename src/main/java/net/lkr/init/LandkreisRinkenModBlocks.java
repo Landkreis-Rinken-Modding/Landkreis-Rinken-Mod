1a5f281dcaf78d3ac57ef6e574ab33b7fa308b39
jeans <jeans@192.168.178.43>
2024-08-10 02:51:52 +0200

    MCreator Merge commit '1bd3410351d5d6938c944a4ee9b5bc0e181f24a6' into 840caec427213ffe6d2689294759da915edf8b8d

--- src/main/java/net/lkr/init/LandkreisRinkenModBlocks.java
+++ src/main/java/net/lkr/init/LandkreisRinkenModBlocks.java
@@ -11,13 +11,11 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraft.world.level.block.Block;
 
 import net.lkr.block.SpindblockBlock;
-import net.lkr.block.JagermeisterFlascheBlock;
 import net.lkr.LandkreisRinkenMod;
 
 public class LandkreisRinkenModBlocks {
 	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, LandkreisRinkenMod.MODID);
 	public static final RegistryObject<Block> SPINDBLOCK = REGISTRY.register("spindblock", () -> new SpindblockBlock());
-	public static final RegistryObject<Block> JAGERMEISTER_FLASCHE = REGISTRY.register("jagermeister_flasche", () -> new JagermeisterFlascheBlock());
 	// Start of user code block custom blocks
 	// End of user code block custom blocks
 }
