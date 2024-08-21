be186d06e130f32209ccfe6e768c70a9f840889e
xXSebiXx <jeans@192.168.178.43>
2024-08-21 04:44:01 +0200

    Tabletten funktionieren

--- src/main/java/net/lkr/item/TablettenItem.java
+++ src/main/java/net/lkr/item/TablettenItem.java
@@ -2,14 +2,15 @@
 package net.lkr.item;
 
 import net.minecraft.world.level.Level;
-import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
 
-import net.lkr.procedures.TablettenOnPlayerStoppedUsingProcedure;
+import net.lkr.procedures.TablettenRightclickedProcedure;
 
 public class TablettenItem extends Item {
 	public TablettenItem() {
@@ -17,17 +18,14 @@ public class TablettenItem extends Item {
 	}
 
 	@Override
-	public UseAnim getUseAnimation(ItemStack itemstack) {
-		return UseAnim.NONE;
-	}
-
-	@Override
 	public int getUseDuration(ItemStack itemstack) {
-		return 0;
+		return 3;
 	}
 
 	@Override
-	public void releaseUsing(ItemStack itemstack, Level world, LivingEntity entity, int time) {
-		TablettenOnPlayerStoppedUsingProcedure.execute(entity);
+	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
+		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
+		TablettenRightclickedProcedure.execute(entity);
+		return ar;
 	}
 }
--- src/main/java/net/lkr/procedures/TablettenRightclickedProcedure.java
+++ src/main/java/net/lkr/procedures/TablettenRightclickedProcedure.java
@@ -5,11 +5,11 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.effect.MobEffectInstance;
 
-public class TablettenOnPlayerStoppedUsingProcedure {
+public class TablettenRightclickedProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
 		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-			_entity.addEffect(new MobEffectInstance(MobEffects.HEALTH_BOOST, 360000, (int) 0.1));
+			_entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 6000, (int) 0.000001));
 	}
 }
